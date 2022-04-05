5a592dac05c1fdce03241d442796beef65a32b3a
Michael Mathu <mathumichael17@gmail.com>
2022-04-05 22:40:02 +0300

    Array creation

--- Program.cs
+++ Program.cs
@@ -3,10 +3,13 @@
     {
         static void Main(string[] args)
         {
-            //Arrays Declaration(named location in the memory to stick staff i.e no arrays in it
+         //Arrays Declaration(named location in the memory to stick staff i.e no arrays in it
          int[] scores;
 
-            
+         //Creating a new array and place it in the variable. We use new keyword and specify the number of elements that you'll have in the array
+        int[] scores = new int[10];
+
+
         }
     }
 }
