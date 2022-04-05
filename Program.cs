004bcc7d1e6c8583ba66b845d796706b3268c9e5
Michael Mathu <mathumichael17@gmail.com>
2022-04-05 22:39:12 +0300

    Array declaration

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Arrays {
+    class Arrays
+    {
+        static void Main(string[] args)
+        {
+            //Arrays Declaration(named location in the memory to stick staff i.e no arrays in it
+         int[] scores;
+
+            
+        }
+    }
+}
