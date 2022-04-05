4be3e59703a7b6a9a5f8dabc8819ba024b7ebcca
Michael Mathu <mathumichael17@gmail.com>
2022-04-06 00:01:50 +0300

    Length of array

--- Program.cs
+++ Program.cs
@@ -3,11 +3,20 @@
     {
         static void Main(string[] args)
         {
-         //Arrays Declaration(named location in the memory to stick staff i.e no arrays in it
-         int[] scores;
+            //Arrays Declaration(named location in the memory to stick staff i.e no arrays in it
+            //int[] scores;
 
-         //Creating a new array and place it in the variable. We use new keyword and specify the number of elements that you'll have in the array
-        int[] scores = new int[10];
+            //Creating a new array and place it in the variable. We use new keyword and specify the number of elements that you'll have in the array
+            //int[] scores = new int[10]; //An array with a rooom for 10 items in it.
+
+
+             //LENGTH OF ARRAY
+            int[] scores = new[] { 100, 90, 80, 70, 60 };
+
+            int totalLengthInArray = scores.Length;
+
+            Console.WriteLine(totalLengthInArray);
+
 
 
         }
